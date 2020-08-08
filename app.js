0421a04b0af8c808c7d9b16db91b40d71a835960
Andrew Collier <atc3598@icloud.com>
2020-08-07 21:25:40 -0400

    fixing bugs

--- app.js
+++ app.js
@@ -9,9 +9,8 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-//created employees array
+
 const employees = [];
-//function that handles the prompts
 const initial = () => {
   inquirer
     .prompt([
@@ -46,116 +45,113 @@ const initial = () => {
       employees.push(newManager);
       moreEmployees();
     });
-
-  const employeeQuestions = () => {
-    inquirer
-      .prompt({
-        type: "list",
-        message: "What type of employee would you like to use?",
-        choices: ["Engineer", "Intern"],
-        name: "role",
-      })
-      .then((answers) => {
-        if (answers.role === "Engineer") {
-          {
-            inquirer
-              .prompt([
-                {
-                  type: "input",
-                  message: "What is engineer's name?",
-                  name: "name",
-                },
-                {
-                  type: "input",
-                  message: "What is engineer's id number?",
-                  name: "id",
-                },
-                {
-                  type: "input",
-                  message: "What is engineer's email?",
-                  name: "email",
-                },
-                {
-                  type: "input",
-                  message: "What is engineer's github username?",
-                  name: "github",
-                },
-              ])
-              .then((response) => {
-                const newEngineer = new Engineer(
-                  response.name,
-                  response.id,
-                  response.email,
-                  response.github
-                );
-                employees.push(newEngineer);
-                moreEmployees();
-              });
-          }
-        } else if (answers.role === "Intern") {
-          {
-            inquirer
-              .prompt([
-                {
-                  type: "input",
-                  message: "What is intern's name?",
-                  name: "name",
-                },
-                {
-                  type: "input",
-                  message: "What is intern's Id number?",
-                  name: "id",
-                },
-                {
-                  type: "input",
-                  message: "What is intern's email?",
-                  name: "email",
-                },
-                {
-                  type: "input",
-                  message: "What is your intern's school?",
-                  name: "school",
-                },
-              ])
-              .then((response) => {
-                const newIntern = new Intern(
-                  response.name,
-                  response.id,
-                  response.email,
-                  response.school
-                );
-                employees.push(newIntern);
-                moreEmployees();
-              });
-          }
-        }
-      });
-  };
 };
-}
-const moreEmployees = ()=>{
-    inquirer.prompt({
-        type: "confirm",
-        message: "Add more employees??",
-        name: "moreEmployees",
-        default: true
-    }).then((answer)=>{
-        if(answer.moreEmployees === true){
-            employeeQuestions()
-        }else{
-            console.log("Your results will be located in the output folder.")
-            const createdTeam = render (employees)
-            fs.writeFile(outputPath, createdTeam, function(err){
-                if(err) throw err
-            } )
-
-        }
+const employeeQuestions = () => {
+  inquirer
+    .prompt({
+      type: "list",
+      message: "What type of employee would you like to use?",
+      choices: ["Engineer", "Intern"],
+      name: "role",
     })
-}
+    .then((answers) => {
+      if (answers.role === "Engineer") {
+        {
+          inquirer
+            .prompt([
+              {
+                type: "input",
+                message: "What is engineer's name?",
+                name: "name",
+              },
+              {
+                type: "input",
+                message: "What is engineer's id number?",
+                name: "id",
+              },
+              {
+                type: "input",
+                message: "What is engineer's email?",
+                name: "email",
+              },
+              {
+                type: "input",
+                message: "What is engineer's github username?",
+                name: "github",
+              },
+            ])
+            .then((response) => {
+              const newEngineer = new Engineer(
+                response.name,
+                response.id,
+                response.email,
+                response.github
+              );
+              employees.push(newEngineer);
+              moreEmployees();
+            });
+        }
+      } else if (answers.role === "Intern") {
+        {
+          inquirer
+            .prompt([
+              {
+                type: "input",
+                message: "What is intern's name?",
+                name: "name",
+              },
+              {
+                type: "input",
+                message: "What is intern's Id number?",
+                name: "id",
+              },
+              {
+                type: "input",
+                message: "What is intern's email?",
+                name: "email",
+              },
+              {
+                type: "input",
+                message: "What is your intern's school?",
+                name: "school",
+              },
+            ])
+            .then((response) => {
+              const newIntern = new Intern(
+                response.name,
+                response.id,
+                response.email,
+                response.school
+              );
+              employees.push(newIntern);
+              moreEmployees();
+            });
+        }
+      }
+    });
+};
+const moreEmployees = () => {
+  inquirer
+    .prompt({
+      type: "confirm",
+      message: "Add more employees??",
+      name: "moreEmployees",
+      default: true,
+    })
+    .then((answer) => {
+      if (answer.moreEmployees === true) {
+        employeeQuestions();
+      } else {
+        console.log("Your results will be located in the output folder.");
+        const createdTeam = render(employees);
+        fs.writeFile(outputPath, createdTeam, function (err) {
+          if (err) throw err;
+        });
+      }
+    });
+};
 
-
-initial()
-
+initial();
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
